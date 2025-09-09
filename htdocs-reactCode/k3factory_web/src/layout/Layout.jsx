8e0dfe421118c4e46897fba35218111cf977e782
skip-row <mempil.default@gmail.com>
2025-09-09 17:33:07 +0900

    메인 헤더/푸터구성

--- htdocs-reactCode/k3factory_web/src/layout/Layout.jsx
+++ htdocs-reactCode/k3factory_web/src/layout/Layout.jsx
@@ -3,19 +3,14 @@ import { Outlet } from "react-router-dom"
 import TotalHeader from "./Header"
 import TotalFooter from "./Footer"
 
-import Dummy from '../data/DummyData.json'
-
-
 export default function Layout() {
   console.log(<Dummy />);
   return (
     <div className="layoutWrap">
-        {/* 
         <TotalHeader />
         <main id="main">
             <Outlet />
         </main>
-        */}
         <TotalFooter />
     </div>
   )
